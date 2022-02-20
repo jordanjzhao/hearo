3988fea2ac6fb7bfc34e03204c82abb7e35e1c09
jordanjzhao <zhao.jordan@hotmail.com>
2022-02-19 16:27:55 -0800

    implementation of SpeechToTextPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import "./index.css";
 import React from "react";
 import LandingPage from "./components/LandingPage";
+import SpeechToTextPage from './components/SpeechToTextPage';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
-
+      <SpeechToTextPage />
     </div>
   );
 }
