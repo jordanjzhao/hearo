3a6478536eaf89c3e6ebf72f4265d69fbd900246
tamjohn <tamjohn@student.ubc.ca>
2022-02-19 22:59:28 -0800

    updated

--- src/components/SpeechToTextPage.js
+++ src/components/SpeechToTextPage.js
@@ -3,8 +3,30 @@ import hearoLogoWhite from "../images/hearoLogoWhite.svg";
 import barVector from "../images/barVector.svg";
 import textDisplay from "../images/textDisplay.svg";
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    textField: {
+        width: "120vh",
+    },
+    center: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: "5vh"
+    }
+})
 
 export default function SpeechToTextPage() {
+    const classes = useStyles();
+    const [inputMsg, setInputMsg] = useState("");
+
+    const handleTypeInputMsg = (event) => {
+        console.log(event.target.value);
+        setInputMsg(event.target.value);
+    }
+
     return (
         <div>
             <header className="header--stt">
@@ -14,7 +36,10 @@ export default function SpeechToTextPage() {
             <main>
                 <h1>Click Link to Start</h1>
                 <div className="stt--container">
-                <textarea rows = "20" cols = "60" id="textbodid" className="stt--textDisplay" placeholder="Text will stream shortly...">
+                <textarea rows = "20" cols = "60" id="textbodid" className="stt--textDisplay" 
+                placeholder="Text will stream shortly..."
+                value={inputMsg}
+                onChange={handleTypeInputMsg}>
                 </textarea>
                 </div>
                 <Link to="/">
